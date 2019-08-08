5c04492125d42d75ccc7f6d0405955e5c57d7cfc
TheWaterIsAlive <ajfry203@gmail.com>
2019-08-09 10:58:39 +1200

    FOrgot

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello World!')
+//var name = 'Allan Fry'
+
+//console.log(name)
+
+
+bla =2;
+var bla;
+
+let name = 'Nelson';
+ name = 'Nz'
+
+console.log(name);
+
+const lastName = 'Fry';
+const firstName = 'Allan';
+
+console.log(lastName);
+console.log(firstName);
